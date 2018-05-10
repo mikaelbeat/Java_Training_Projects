40077100333c1de173ff9f7086c7e900abab953c
Ryynänen,Petri <petri.ryynanen@accenture.com>
2018-05-10 15:08:44 +0300

    Update

--- Java_Training_Projects/src/file_Training/File_Training.java
+++ Java_Training_Projects/src/file_Training/File_Training.java
@@ -1,25 +1,61 @@
 package file_Training;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class File_Training {
+	
+	static boolean quit = false;
+	
+	static String filename = "C:\\Users\\petri.ryynanen\\git\\Java_Training_Projects\\Java_Training_Projects\\src\\file_Training\\dataFile.txt";
+	static String something;
+	static File file = new File(filename);
+	
+	static Scanner reader = new Scanner(System.in);
 
 	public static void main(String[] args) {
+	
+		int choice = 0;
+		menu();
 		
-		String filename = "C:\\Users\\petri.ryynanen\\eclipse-workspace\\PassingTime\\src\\dataFile.txt";
-		String something;
-		File file = new File(filename);
+		while(!quit) {
+			choice = reader.nextInt();
+			System.out.println();
+			reader.nextLine();
+			System.out.println("");
+			
+			switch(choice) {
+			case 0:
+//				menu();
+				break;
+			case 1:
+				writeToFile();
+				break;
+			case 2:
+//				printFileContent();
+				break;
+			case 3:
+//				deleteFileContent();
+				break;
+			case 4:
+//				quit();
+				quit = true;
+			}
+		}
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter something: ");
-		something = sc.nextLine();
+
+	}
+	
+	public static void writeToFile() {
+		System.out.print("Enter something to write to file: ");
+		something = reader.nextLine();
 		try {
-			FileWriter fw = new FileWriter(file);
-			fw.write(something);
-			fw.close();
+			FileWriter fw = new FileWriter(file, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(something+"\n");
+			bw.close();
 		} catch (IOException e) {
 			System.out.println("Cannot write to file!");
 			e.printStackTrace();
@@ -27,9 +63,14 @@ public class File_Training {
 			System.out.println("Writed "+something+" to file");
 			System.out.println("Closing file!");
 		}
-		
-		System.out.print(something);
-
+	}
+	
+	public static  void menu() {
+		System.out.println
+				("1 - Write password to file.\n"+
+				 "2 - Check passwords valitidy in file.\n"+
+				 "3 - Show password file content.\n"+
+				   "4 - Quit");
 	}
 
 }
